f82aca149a12026a09034b302c5fd4fd4d7a8fe1
Joey Adams <joeyadams3.14159@gmail.com>
2012-04-02 00:50:54 -0400

    Display a tree view using Gtk2Hs
    Issues:
    
     * Columns don't auto-expand.
    
     * Scroll bar does not appear.
    
     * Column cannot be resized.
    
     * Total is not displayed.
    
     * Code is more repetitive and verbose than it needs to be.

--- Main.hs
+++ Main.hs
@@ -1,8 +1,49 @@
+{-# LANGUAGE RecordWildCards #-}
 import CountLines
-import Data.Tree
+import Graphics.UI.Gtk
 
 main :: IO ()
 main = do
     (forest, total) <- fmap lines getContents >>= countLines
-    putStr $ drawForest $ map (fmap show) forest
-    putStrLn $ "Total line count: " ++ show total
+
+    -- Display the tree on the console
+    --      putStr $ drawForest $ map (fmap show) forest
+    --      putStrLn $ "Total line count: " ++ show total
+
+    -- The following is based on TreeDemo.hs in the
+    -- Gtk2Hs source distribution.
+
+    _ <- initGUI
+
+    win <- windowNew
+    _ <- onDestroy win mainQuit
+
+    model <- treeStoreNew forest
+    view  <- treeViewNewWithModel model
+
+    treeViewSetHeadersVisible view True
+
+    colFileName      <- treeViewColumnNew
+    colNumberOfLines <- treeViewColumnNew
+
+    treeViewColumnSetTitle colFileName      "Name"
+    treeViewColumnSetTitle colNumberOfLines "# Lines"
+
+    rendererFileName      <- cellRendererTextNew
+    rendererNumberOfLines <- cellRendererTextNew
+
+    cellLayoutPackStart colFileName         rendererFileName        True
+    cellLayoutPackStart colNumberOfLines    rendererNumberOfLines   True
+
+    cellLayoutSetAttributes colFileName rendererFileName model $ \Entry{..} ->
+        [ cellText := entryName ]
+    cellLayoutSetAttributes colNumberOfLines rendererNumberOfLines model $ \Entry{..} ->
+        [ cellText := show entryLineCount ]
+
+    _ <- treeViewAppendColumn view colFileName
+    _ <- treeViewAppendColumn view colNumberOfLines
+
+    containerAdd win view
+
+    widgetShowAll win
+    mainGUI
